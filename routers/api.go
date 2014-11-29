e0c2c3f335f9ef7d38b50e281c39288a40d60673
mnhkahn <lichao0407@gmail.com>
2014-11-29 14:58:53 +0800

    remove tv, car,ip & weather

--- routers/api.go
+++ routers/api.go
@@ -10,14 +10,8 @@ import (
 func init() {
 	beego.Errorhandler("404", page_not_found)
 	beego.Router("/post", &controllers.CyeamController{}, "*:GetPost")
-	beego.Router("/ip", &controllers.IpController{}, "*:GetIp")
-	beego.Router("/weather", &controllers.WeatherController{}, "*:GetWeather")
-	beego.Router("/tv", &controllers.TVController{})
 	beego.Router("/doodle", &controllers.DoodleController{})
 	beego.Router("/bing", &controllers.BingController{})
-	beego.Router("/car", &controllers.CarController{})
-	beego.Router("/time", &controllers.TestController{}, "*:Time")
-
 }
 
 var errtol = "<iframe scrolling='no' frameborder='0' src='http://yibo.iyiyun.com/js/yibo404/key/2354' width='640' height='464' style='display:block;'></iframe>"
